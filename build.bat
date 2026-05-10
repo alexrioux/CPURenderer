a33af33b92543a5fed8387b082a16e4c058470de
Axairlem <Axairlem@gmail.com>
2026-05-10 15:15:18 -0400

    Remove redundant variable initialisation

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat" && cl /Fe:CPURenderer *.cpp /Zi /WX /link user32.lib Gdi32.lib
+cl /Fe:CPURenderer *.cpp /Zi /WX /link user32.lib Gdi32.lib
 pause
