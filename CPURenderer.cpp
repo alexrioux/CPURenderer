a99559167306c855fbe1e419e667b711f9d730ef
Axairlem <Axairlem@gmail.com>
2026-05-11 02:15:52 -0400

    Add gradient render loop

--- CPURenderer.cpp
+++ CPURenderer.cpp
@@ -17,19 +17,85 @@ long long AbsoluteValue(long long value)
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    PAINTSTRUCT paintStruct     = {};
-    HDC deviceContext           = {};
-
     switch (message)
     {
         case WM_PAINT:
         {
-            OutputDebugString(WIN32_STR("PAINT msg received\n"));
+            PAINTSTRUCT paintStruct             = {};
+            HDC deviceContext                   = {};
+            deviceContext                       = BeginPaint(hWnd, &paintStruct);
+            const DWORD bitmapWidth             = 1001;
+            const DWORD bitmapHeight            = 1000;
+            const unsigned short bitsPerPixel   = 24;
+            const unsigned int bitsPerRow       = bitmapWidth * bitsPerPixel;
+            int bitMask                         = (int)0b11111111111111111111111111100000;
+            const unsigned int bitStride        = (bitsPerRow + 31) & bitMask; // add just enough to mask the unused bits
+            const unsigned int byteStride       = bitStride >> 3; // divide by 2^3
 
-            deviceContext       = BeginPaint(hWnd, &paintStruct);
-            WIN32_STRPTR text   = WIN32_STR("Text Out");
-            TextOut(deviceContext, 5, 5, text, ARRAY_COUNT(text));
+            // CREATE BITMAPINFO
+            BITMAPINFOHEADER bitmapHeader       = {};
+            bitmapHeader.biSize                 = sizeof(BITMAPINFOHEADER);
+            bitmapHeader.biWidth                = bitmapWidth;
+            bitmapHeader.biHeight               = -bitmapHeight; // origin @ upper-left corner
+            bitmapHeader.biPlanes               = 1;
+            bitmapHeader.biBitCount             = bitsPerPixel;
+            bitmapHeader.biCompression          = BI_RGB;
+            bitmapHeader.biSizeImage            = AbsoluteValue(bitmapHeader.biHeight) * byteStride;
+            bitmapHeader.biXPelsPerMeter        = 0;
+            bitmapHeader.biYPelsPerMeter        = 0;
+            bitmapHeader.biClrUsed              = 0;
+            bitmapHeader.biClrImportant         = 0;
+            BITMAPINFO bitmapInfo               = {};
+            bitmapInfo.bmiHeader                = bitmapHeader;
+            void* pixelData                     = 0;
+            int colorUse                        = DIB_RGB_COLORS;
+            // CREATE DIB & GET PIXELDATA POINTER
+            HBITMAP bitmapHandle = CreateDIBSection(
+                deviceContext,
+                &bitmapInfo,
+                colorUse,
+                &pixelData,
+                0,
+                0
+            );
+            if (!bitmapHandle || !pixelData)
+            {
+                OutputDebugString(WIN32_STR("Failed to create DIB section"));
+                return -1;
+            }
+
+            // RENDER PIXELDATA
+            for (int scanLine = 0; scanLine < bitmapHeight; scanLine++)
+            {
+                for (int pixel = 0; pixel < byteStride; pixel += 3)
+                {
+                    int currentByte = (scanLine * byteStride) + pixel;
+
+                    float t = (float)pixel / (float)byteStride;
+                    int value = 255.0f * t;
+
+                    ((unsigned char*)pixelData)[currentByte]      = (unsigned char)(value);   // B
+                    ((unsigned char*)pixelData)[currentByte + 1]  = (unsigned char)(0x00);    // G
+                    ((unsigned char*)pixelData)[currentByte + 2]  = (unsigned char)(0x00);    // R
+                }
+            }
+
+            // SET DIB's PIXEL DATA
+            int linesDrawn = SetDIBitsToDevice(
+                deviceContext,
+                0, 0,
+                bitmapWidth, bitmapHeight,
+                0, 0,
+                0,
+                bitmapHeight,
+                pixelData,
+                &bitmapInfo,
+                colorUse
+            );
+
             EndPaint(hWnd, &paintStruct);
+
+            DeleteObject(bitmapHandle); // TODO - reuse the same bitmap to avoid allocs
         }
         break;
         case WM_DESTROY:
