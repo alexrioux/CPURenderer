044a52949d1504bb94aeadf6c6a5428195342599
Axairlem <Axairlem@gmail.com>
2026-05-17 22:31:20 -0400

    Add pause on error & exit on valid
    Prevents having to press enter every time a build completes, while pausing whenever an error occur.

--- build.bat
+++ build.bat
@@ -2,6 +2,12 @@
 set compilerOptions=/Fe:CPURenderer /Zi /W3 /WX /EHa- /GS-
 set linkerOptions=/SUBSYSTEM:WINDOWS /ENTRY:Win32EntryPoint /NODEFAULTLIB /OPT:REF
 set libraries=user32.lib Gdi32.lib kernel32.lib
-pause
 set defines=/DWIN32 /DUNICODE
 cl %defines% *.cpp %compilerOptions% /link %linkerOptions% %libraries%
+
+@if %ERRORLEVEL% NEQ 0 (
+    echo "Failed to build the application"
+    pause
+) else (
+    exit /b
+)
