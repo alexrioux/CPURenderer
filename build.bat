e37435558a8b336f0fff9fd044fc511c17ebdea9
Axairlem <Axairlem@gmail.com>
2026-05-17 22:29:39 -0400

    Add defines for WIN32 & UNICODE

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@
 set compilerOptions=/Fe:CPURenderer /Zi /W3 /WX /EHa- /GS-
 set linkerOptions=/SUBSYSTEM:WINDOWS /ENTRY:Win32EntryPoint /NODEFAULTLIB /OPT:REF
 set libraries=user32.lib Gdi32.lib kernel32.lib
-cl *.cpp %compilerOptions% /link %linkerOptions% %libraries%
 pause
+set defines=/DWIN32 /DUNICODE
+cl %defines% *.cpp %compilerOptions% /link %linkerOptions% %libraries%
