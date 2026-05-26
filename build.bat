30d58066072ad4cbaa0d301ae4ceff842bd9424a
Axairlem <Axairlem@gmail.com>
2026-05-26 14:27:06 -0400

    Add DEBUG define
    To omit certain function definitions based on the target compiling mode.

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 set compilerOptions=/Fe:CPURenderer /Zi /W3 /WX /EHa- /GS-
 set linkerOptions=/SUBSYSTEM:WINDOWS /ENTRY:Win32EntryPoint /NODEFAULTLIB /OPT:REF
 set libraries=user32.lib Gdi32.lib kernel32.lib
+set defines=/DWIN32 /DDEBUG
 cl %defines% *.cpp %compilerOptions% /link %linkerOptions% %libraries%
 
 @if %ERRORLEVEL% NEQ 0 (
