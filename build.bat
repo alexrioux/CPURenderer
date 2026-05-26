7adc5b8b49cbe7a7a2c2c81a6d37238cf9668d6b
Axairlem <Axairlem@gmail.com>
2026-05-26 14:25:05 -0400

    Remove UNICODE
    The code now uses exclusively UTF-8 encoding and the "wide" versions of win32 calls.

--- build.bat
+++ build.bat
@@ -2,7 +2,6 @@
 set compilerOptions=/Fe:CPURenderer /Zi /W3 /WX /EHa- /GS-
 set linkerOptions=/SUBSYSTEM:WINDOWS /ENTRY:Win32EntryPoint /NODEFAULTLIB /OPT:REF
 set libraries=user32.lib Gdi32.lib kernel32.lib
-set defines=/DWIN32 /DUNICODE
 cl %defines% *.cpp %compilerOptions% /link %linkerOptions% %libraries%
 
 @if %ERRORLEVEL% NEQ 0 (
