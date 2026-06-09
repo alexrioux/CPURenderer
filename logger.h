542dea39c28cd1bd6dea82aba91f7d6cc22bd481
Axairlem <Axairlem@gmail.com>
2026-06-09 17:52:22 -0400

    Add signed integer logging
    Changed raw pointer params to reference of array to improve type safety.

--- logger.h
+++ logger.h
@@ -8,7 +8,7 @@ void PlatformLogInfo(const char* str, ulong size);
 void PlatformLogWarning(const char* str, ulong size);
 void PlatformLogError(const char* str, ulong size);
 
-static unsigned int UIntToStr(unsigned int integer, char* dst)
+static unsigned int UIntToStr(unsigned int integer, char (&dst)[10])
 {
     // TODO - extract 2 digits at a time
 
@@ -32,6 +32,21 @@ static unsigned int UIntToStr(unsigned int integer, char* dst)
     return bytesProcessed;
 }
 
+static unsigned int IntToStr(int integer, char (&dst)[11])
+{
+    if (0x80000000 & integer) // Negative
+    {
+        unsigned int digitCount = UIntToStr((~integer) + 1, *(char (*)[10])(&dst[1]));
+        dst[10 - digitCount] = '-';
+
+        return digitCount + 1; // +1 for sign
+    }
+    else // Positive
+    {
+        return UIntToStr((unsigned int)integer, *(char (*)[10])(&dst[1]));
+    }
+}
+
 static ulong StringCopy(const char* src, char* dst, ulong bytesToCopy) // Returns bytes written
 {
     ulong bytesWritten = 0;
@@ -41,6 +56,12 @@ static ulong StringCopy(const char* src, char* dst, ulong bytesToCopy) // Return
     }
     return bytesWritten;
 }
+
+template<typename T>    static constexpr ulong g_typeLength                 = 0;
+template<ulong N>       static constexpr ulong g_typeLength<const char[N]>  = N - 1; // N - 1 to avoid copying '\0'
+//template<>              static constexpr ulong g_typeLength<char>           = 1;
+template<>              static constexpr ulong g_typeLength<unsigned int>   = 10;
+template<>              static constexpr ulong g_typeLength<int>   = 10;
 
 template<ulong N> static inline ulong AppendArg(const char (&strArg)[N], char* dst) // Returns bytes written
 {
@@ -54,9 +75,13 @@ static inline ulong AppendArg(unsigned int intArg, char* dst) // Returns bytes w
     ulong bytesWritten      = StringCopy(strBuffer + (10 - digitCount), dst, digitCount); // strBuffer is filled in reverse
     return bytesWritten;
 }
-template<typename T>    static constexpr ulong g_typeLength                 = 0;
-template<>              static constexpr ulong g_typeLength<unsigned int>   = 10;
-template<ulong N>       static constexpr ulong g_typeLength<const char[N]>  = N - 1; // N - 1 to avoid copying '\0'
+static inline ulong AppendArg(int intArg, char* dst) // Returns bytes written
+{
+    char strBuffer[11];
+    unsigned int digitCount = IntToStr(intArg, strBuffer);
+    ulong bytesWritten      = StringCopy(strBuffer + (11 - digitCount), dst, digitCount); // strBuffer is filled in reverse
+    return bytesWritten;
+}
 
 template<typename... ArgT> void LogInfo(ArgT&... arg)
 {
